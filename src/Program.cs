68750e7a56563edf3b5db5866f5599392089d9c1
Felipe Martins dos Santos <felipemsanto2@gmail.com>
2025-10-27 14:39:22 -0300

    Etapas realizadas primeiro desafio...

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,11 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(op => 
+   op.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
